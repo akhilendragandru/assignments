f2f33fa12aff4d87a0f222e0dfe57ad285ade1d4
akhilendragandru <98321681+akhilendragandru@users.noreply.github.com>
2022-02-04 09:27:05 +0530

    Add files via upload

---
+++ stationary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stationary
+{
+    class Stationary
+    {
+        static void Main()
+        {
+            List<string> Stat = new List<string>();
+            Stat.Add("Pencil");
+            Stat.Add("Pen");
+            Stat.Add("Scale");
+            Stat.Add("Sharpner");
+            Stat.Add("dot");
+            Stat.Add("Book");
+            Console.WriteLine("The Added Items are:");
+            foreach (var item in Stat)
+            {
+
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
